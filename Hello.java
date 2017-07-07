f17720bbf0c7e3216bbe1817b327f8d858c7b650
anubhavcse08 <anubhavcse08@users.noreply.github.com>
2017-07-07 19:04:58 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+      public static void main(String args[])
+      {
+    	  int i=1;
+    	  for(i=1;i<=5;i++)
+    	  {
+    	  System.out.println("hello");
+    	  }
+      }
+}
