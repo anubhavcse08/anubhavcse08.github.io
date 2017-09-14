eb1e7e23b5095a9dd976bcc2e63226dd1fae5a4e
Anubhav kumar <anubhavcse08@users.noreply.github.com>
2017-09-14 05:19:34 -0700

    Add files via upload

---
+++ ReverseDigit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ReverseDigit {
+	public static void main(String args[]){
+		int n,rem,b=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" enter the number : ");
+		n= sc.nextInt();
+		while(n>0)
+		{
+			rem=n%10;
+			b=b*10+rem;
+			n=n/10;
+		}
+		System.out.println(" the reverse number : " +b);
+		sc.close();
+	}
+}
